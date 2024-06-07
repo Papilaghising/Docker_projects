5064a8ab980a67e1a17b2e78069e8bbecada03dc
pailaghising <papilaghising1111@gmail.com>
2024-06-07 12:45:15 +0545

    added changesclear

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,5 +25,22 @@ module "ecr" {
       }]
   }
   repository_read_write_access_arns = var.repository_read_write_access_arns
+  repository_lifecycle_policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1,
+        description  = "Keep last 30 images",
+        selection = {
+          tagStatus     = "tagged",
+          tagPrefixList = ["v"],
+          countType     = "imageCountMoreThan",
+          countNumber   = 30
+        },
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
 }
 
